e70e3f2f3aaa21139d563d5d12e1b314f002d80d
Bhagya Unnikrishnan <bhagyanandhu2002@gmail.com>
2023-03-31 22:04:57 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ var typed = new Typed(".multiple-text", {
     backDelay: 1000,
     loop: true
 })
+
+/* scroll section active link*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
