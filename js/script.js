e351b6b1007c96e9becfda19c3dad9518fe7a713
Bhagya Unnikrishnan <bhagyanandhu2002@gmail.com>
2023-04-02 14:12:34 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -6,6 +6,15 @@ var typed = new Typed(".multiple-text", {
     loop: true
 })
 
+/* toggle icon navbar*/
+let menuIcon  = document.querySelector('#menu-icon');
+let navbar  = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
 /* scroll section active link*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -24,4 +33,12 @@ window.onscroll = () => {
             });
         };
     });
+    /* sticky navbar */
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /* remove toggle icon and navbar when click navbar link(scroll)*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
